104546809df81da41b067c3eaa89332066648890
AShiningRay <genesis45.ppc@gmail.com>
2023-02-21 14:11:17 -0300

    Add updated MMR algorithm, and results.

--- MMR/main_mmr.py
+++ MMR/main_mmr.py
@@ -2,9 +2,9 @@ import mmr_summarize
 import os
 
 mmr = mmr_summarize.mmr_summarize()
-summary_length = 50 # Summary will contain 50 sentences
+summary_length = 45 # Summary will contain more or less 50 sentences, due to the algorithm's small variance in the amount of selected sentences
 summary = []
-query_length = 25
+query_length = 10
 summ_Folder = "/content/drive/MyDrive/Proj_PFC/MMR/summaries/"
 Root_Folder = "/content/drive/MyDrive/Proj_PFC/MMR/documents/"
 Directory_List = next(os.walk(Root_Folder))[1]
@@ -18,7 +18,7 @@ for Directory in Directory_List:
         data = ""
         summary = mmr.main(query_length, summary_length, root_dir)          
         for sent in summary:
-            data +=  sent.getOGwords() + " "
+            data +=  sent.getOGwords() + "\n"
 
         FileName = summ_Folder +Directory+ "_MMR.txt"
         print(FileName)
